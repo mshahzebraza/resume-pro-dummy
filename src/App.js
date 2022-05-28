d87e1ed89d435c67b9d0264af24531de21b991b6
mshahzebraza97 <m.shahzebraza.97@gmail.com>
2022-05-28 22:01:42 +0500

    To Components

--- src/App.js
+++ src/App.js
@@ -2,58 +2,49 @@ import logo from './logo.svg';
 import './App.scss';
 import Layout from './components/Layout/Layout';
 
+
+function InputBanner() {
+     return (<div className="inputBanner">
+          <p>Drag and Drop Files here</p>
+          <p>OR</p>
+          <button className='primaryBtn'>Browse Files</button>
+     </div>);
+}
+
+
+
+function Descript() {
+     return (<div className="description">
+          <p>
+               Being easy to use doesn't have to mean that you are forced into using a pre-made template. Make your resume stand out from the pack by trying different colours, fonts, headline formats and much more.
+          </p>
+     </div>);
+}
+
+
+
+function Gallery() {
+     return (<section className="templateGallery">
+          <div className="template"></div>
+          <div className="template"></div>
+          <div className="template"></div>
+          <div className="template"></div>
+          <div className="template"></div>
+          <div className="template"></div>
+     </section>);
+}
+
+
 function App() {
      return (
           <div className="App">
                <Layout />
                <main>
-                    <section className="intro">
-
-                         <h1 className="introTitle">
-                              Job Winning Resume
-                         </h1>
-                         <h3 className="introSubTitle">
-                              Being easy to use doesn't have to mean that you are forced into using a pre-made template. Make your resume stand out from the pack by trying different colours, fonts, headline formats and much more.
-                         </h3>
-                    </section>
-                    <nav className="nav">
-                         <ul className="ctgList">
-                              <li className="ctg">Category 1</li>
-                              <li className="ctg">Category 2</li>
-                              <li className="ctg">Category 3</li>
-                              <li className="ctg">Category 4</li>
-                              <li className="ctg">Category 5</li>
-                              <li className="ctg">Category 6</li>
-                         </ul>
-                         <div className="searchBox">
-                              <i className="searchIcon"></i>
-                              <input type="text" placeholder='Search' className='searchInput' />
-                         </div>
-                         <div className="navMenu">
-
-                              <div className="menuIcon">
-                              </div>
-
-                         </div>
-                    </nav>
-                    <div className="inputBanner">
-                         <p>Drag and Drop Files here</p>
-                         <p>OR</p>
-                         <button className='primaryBtn'>Browse Files</button>
-                    </div>
-                    <div className="description">
-                         <p>
-                              Being easy to use doesn't have to mean that you are forced into using a pre-made template. Make your resume stand out from the pack by trying different colours, fonts, headline formats and much more.
-                         </p>
-                    </div>
-                    <section className="templateGallery">
-                         <div className="template"></div>
-                         <div className="template"></div>
-                         <div className="template"></div>
-                         <div className="template"></div>
-                         <div className="template"></div>
-                         <div className="template"></div>
-                    </section>
+                    <Intro />
+                    <NavBar />
+                    <InputBanner />
+                    <Descript />
+                    <Gallery />
 
                </main>
           </div>
@@ -61,3 +52,39 @@ function App() {
 }
 
 export default App;
+
+function NavBar() {
+     return <nav className="nav">
+          <ul className="ctgList">
+               <li className="ctg">Category 1</li>
+               <li className="ctg">Category 2</li>
+               <li className="ctg">Category 3</li>
+               <li className="ctg">Category 4</li>
+               <li className="ctg">Category 5</li>
+               <li className="ctg">Category 6</li>
+          </ul>
+          <div className="searchBox">
+               <i className="searchIcon"></i>
+               <input type="text" placeholder='Search' className='searchInput' />
+          </div>
+          <div className="navMenu">
+
+               <div className="menuIcon">
+               </div>
+
+          </div>
+     </nav>;
+}
+
+function Intro() {
+     return <section className="intro">
+
+          <h1 className="introTitle">
+               Job Winning Resume
+          </h1>
+          <h3 className="introSubTitle">
+               Being easy to use doesn't have to mean that you are forced into using a pre-made template. Make your resume stand out from the pack by trying different colours, fonts, headline formats and much more.
+          </h3>
+     </section>;
+}
+
